51d4d5143cdb529985317cca44c45cfeca8395fd
alexis779 <alexis@apache.org>
2025-06-10 12:10:00 -0700

    use follower arm state instead of leader arm state to replay episode, call simulator PD controller

--- scripts/sim/replay_episode.py
+++ scripts/sim/replay_episode.py
@@ -1,8 +1,15 @@
+import argparse
 from slobot.configuration import Configuration
 from slobot.lerobot.episode_replayer import EpisodeReplayer
 
+parser = argparse.ArgumentParser(description="Replay a specific episode from a dataset.")
+parser.add_argument("--dataset_repo_id", type=str, required=True, help="Hugging Face Hub repository ID of the dataset.")
+parser.add_argument("--episode_id", type=int, required=True, help="ID of the episode to replay.")
+
+args = parser.parse_args()
+
 mjcf_path = Configuration.MJCF_CONFIG
-repo_id = "alexis779/so100_ball_cup"
+repo_id = args.dataset_repo_id
 episode_replayer = EpisodeReplayer(repo_id=repo_id, mjcf_path=mjcf_path)
-episode_id = 1
+episode_id = args.episode_id - 1
 episode_replayer.replay_episode(episode_id)
