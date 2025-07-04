bbb0fe90ec3c29066c6125cadc794694ce814366
alexis779 <alexis@apache.org>
2025-07-03 23:47:36 -0700

    decouple loading episode data from replaying them

--- scripts/sim/replay_episodes.py
+++ scripts/sim/replay_episodes.py
@@ -3,23 +3,24 @@ from slobot.configuration import Configuration
 from slobot.lerobot.episode_replayer import EpisodeReplayer
 
 parser = argparse.ArgumentParser(description="Replay a specific episode from a dataset.")
-parser.add_argument("--dataset_repo_id", type=str, required=True, help="Hugging Face Hub repository ID of the dataset.")
-parser.add_argument("--episode_id", type=str, default=None, help="Specific episode ID or comma-separated list of IDs to replay (optional).")
+parser.add_argument("--dataset-repo-id", type=str, required=True, help="Hugging Face Hub repository ID of the dataset.")
+parser.add_argument("--episode-ids", type=str, default=None, help="Specific episode ID or comma-separated list of IDs to replay (optional).")
 
 args = parser.parse_args()
 
 mjcf_path = Configuration.MJCF_CONFIG
 repo_id = args.dataset_repo_id
-episode_replayer = EpisodeReplayer(repo_id=repo_id, mjcf_path=mjcf_path, show_viewer=True)
 
-
+episode_count = 1
 episode_ids = None
-
-episode_id_str = args.episode_id
+episode_id_str = args.episode_ids
 if episode_id_str is not None:
     episode_ids = [ int(episode_id)
         for episode_id in episode_id_str.split(',')
     ]
+    episode_count = len(episode_ids)
 
-rate = episode_replayer.replay_episodes(episode_ids)
-print(f"Success rate: {rate:.2f}")
+episode_replayer = EpisodeReplayer(repo_id=repo_id, mjcf_path=mjcf_path, show_viewer=True, n_envs=episode_count)
+episode_replayer.load_episodes(episode_ids=episode_ids)
+rate = episode_replayer.replay_episodes()
+print(f"Success rate: {rate:.2f} for episode_ids = {episode_ids}")
